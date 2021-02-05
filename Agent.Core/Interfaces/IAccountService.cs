64ad2c47af499f8879ab5eb09c6c03f93b6f974c
Thiha Kyaw Htin <thihakyaw.htin@haulio.io>
2021-02-05 08:44:15 +0630

    onepay account service

---
+++ Agent.Core/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agent.Core.Interfaces
+{
+    public interface IAccountService
+    {
+
+    }
+}
---
+++ Agent.Infrastructure/OnePay/AccountService.cs
@@ -0,0 +1,11 @@
+﻿using Agent.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agent.Infrastructure.OnePay
+{
+    public class AccountService : IAccountService
+    {
+    }
+}
