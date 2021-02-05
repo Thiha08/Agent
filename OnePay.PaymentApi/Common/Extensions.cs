e945b09afc279ecb400b191937ce9f8f24fb1763
Thiha Kyaw Htin <thihakyaw.htin@haulio.io>
2021-02-05 10:59:38 +0630

    onepay payment api integration

---
+++ OnePay.PaymentApi/Common/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using OnePay.PaymentApi.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnePay.PaymentApi.Common
+{
+    public static class Extensions
+    {
+        public static PaymentResponse GetSystemErrorResponse(this PaymentRequest value)
+        {
+            return new PaymentResponse
+            {
+                RespCode = PaymentResponseCode.SystemError
+            };
+        }
+    }
+}
--- OnePay.PaymentApi/PaymentService.cs
+++ OnePay.PaymentApi/PaymentService.cs
@@ -1,14 +1,43 @@
-﻿using OnePay.PaymentApi.Dtos;
+﻿using Agent.Core.Constants.OnePay;
+using Agent.Core.Extensions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using OnePay.PaymentApi.Common;
+using OnePay.PaymentApi.Dtos;
 using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace OnePay.PaymentApi
 {
     public class PaymentService : IPaymentService
     {
-        public Task<PaymentResponse> MakePaymentAsync(PaymentRequest request)
+        private IOnePayApiSettings _onePayApiSettings;
+        private readonly ILogger<PaymentService> _logger;
+
+        public async Task<PaymentResponse> MakePaymentAsync(PaymentRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                request.HashValue = Hashing.GetHMAC(request.GetSignatureString(), _onePayApiSettings.Token);
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                string requestUri = _onePayApiSettings.BaseUrl + _onePayApiSettings.DirectPaymentUrl;
+
+                using var httpClient = new HttpClient();
+                using var response = await httpClient.PostAsync(requestUri, content);
+                string responseString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<PaymentResponse>(responseString);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(
+                    "MakePayment  is failed." + Environment.NewLine +
+                    exception.Message);
+
+                return request.GetSystemErrorResponse();
+            }
         }
     }
 }
